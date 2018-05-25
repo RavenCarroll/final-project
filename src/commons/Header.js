75e8c9c7548387a94ef628d867e6de60b7b9ebde
Raven Carroll <raven.acarroll@gmail.com>
2018-05-24 17:57:07 -0700

    finalchanges

---
+++ src/commons/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Header = () => (
+<nav className="navbar navbar-light bg-light">
+  <p className="navbar-brand" href="#">Final Project
+    <img src="/assets/brand/bootstrap-solid.svg" width="30" height="30" alt=""/>
+  </p>
+</nav>
+)
+export default Header
